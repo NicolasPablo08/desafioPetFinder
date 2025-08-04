9894c45ed0c80d2a1c5530be57fdd4ebb2924901
Pablo Bru <pablobru08@gmail.com>
2025-08-03 22:56:15 -0300

    four commit pc

--- fe-src/components/header-comp.ts
+++ fe-src/components/header-comp.ts
@@ -1,17 +1,53 @@
 export function headerComp() {
-	class HeaderComp extends HTMLElement {
-		constructor() {
-			super();
-			this.render();
-		}
-		render() {
-			const shadow = this.attachShadow({ mode: "open" });
-			const div = document.createElement("div");
-			const style = document.createElement("style");
+  class HeaderComp extends HTMLElement {
+    constructor() {
+      super();
+      this.render();
+    }
+    render() {
+      const shadow = this.attachShadow({ mode: "open" });
+      const div = document.createElement("div");
+      div.classList.add("header-container");
+      const style = document.createElement("style");
+      div.innerHTML = `
+			<div class= "header__logo-container">
+				<img class="logo" src="../icons/icon-header.png" alt="Logo">
+			</div>
+			<div class ="header__menu-container">
+			<button class="menu__button-open">
+			<img class="button__open-icon">
+			</button>
+			<div class="menu-window">
+				<button class="menu__button-close">
+					<img class="button__close-icon">
+				</button>
+				<div class="menu-options">
+					<div class="option">
+						<a	href="" class="option-dates text"> Mis datos</a> 
+						<a	href="" class="option-my-reports text"> Mis mascotas reportadas</a>
+						<a	href="" class="option-report text"> Reportar mascota</a>
+					</div>
+					<div class="option-footer">
+						<a href="" class="option-contact">email@gmail.com</a>
+						<a href="" class="option-logout">CERRAR SESIÓN</a>	
+					</div>
+				</div>	
+			</div>	
+			`;
+      style.innerHTML = `
+			.header-container {
+			width: 100%;
+			height: 60px;
+			display: flex;
+			flex-direction: row;
+			justify-content: space-around;
+			background-color: #26302E;
+				}
+			`;
 
-			shadow.appendChild(style);
-			shadow.appendChild(div);
-		}
-	}
-	customElements.define("header-comp", HeaderComp);
+      shadow.appendChild(style);
+      shadow.appendChild(div);
+    }
+  }
+  customElements.define("header-comp", HeaderComp);
 }
--- fe-src/index.ts
+++ fe-src/index.ts
@@ -2,9 +2,11 @@ import { buttonComp } from "./components/button-comp";
 import { textComp } from "./components/text-comp";
 import { inputComp } from "./components/input-comp";
 import { cardComp } from "./components/card-comp";
+import { headerComp } from "./components/header-comp";
 (function main() {
-	buttonComp();
-	textComp();
-	inputComp();
-	cardComp();
+  buttonComp();
+  textComp();
+  inputComp();
+  cardComp();
+  headerComp();
 })();
