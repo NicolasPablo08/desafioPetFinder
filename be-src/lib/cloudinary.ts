9bf672057c546fd452859bbddd580e332f7407a8
Pablo Bru <160796417+NicolasPablo08@users.noreply.github.com>
2025-08-13 15:46:12 -0300

    Delete be-src/lib/cloudinary.ts

--- be-src/lib/cloudinary.ts
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-	cloud_name: "ddnrhrh3q",
-	api_key: "344622688843456",
-	api_secret: "uRYMzreMDbJJSkiHjbaJ8LCQNSQ",
-});
-
-export default cloudinary;
