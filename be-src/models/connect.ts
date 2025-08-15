c678dfd44e6c7d03328730af5eb2f604d61574ca
Pablo Bru <pablobru08@gmail.com>
2025-08-14 22:21:44 -0300

    commit from pc with .env

--- be-src/models/connect.ts
+++ be-src/models/connect.ts
@@ -1,6 +1,3 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-	process.env.SEQUELIZE_URL ||
-		"postgresql://neondb_owner:npg_RblSr8goOM2i@ep-polished-violet-acmaqxn1-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-);
+export const sequelize = new Sequelize(process.env.SEQUELIZE_URL, { dialect: "postgres" });
---
+++ dev.ts
@@ -0,0 +1,3 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import "./be-src/server";
